149c38d285c4b7938be4f35108d825074d2a25df
Miguel Abela <126154045+miguelabela03@users.noreply.github.com>
2026-03-09 14:25:49 +0100

    Added bucket storage upload method

--- pftc-auth/Services/BucketStorageService.cs
+++ pftc-auth/Services/BucketStorageService.cs
@@ -21,9 +21,47 @@ namespace pftc_auth.Services
             throw new NotImplementedException();
         }
 
-        public Task<string> UploadFileAsync(IFormFile file, string fileNameForStorage)
+        public async Task<string> UploadFileAsync(IFormFile file, string fileNameForStorage)
         {
-            throw new NotImplementedException();
+            if (file == null || file.Length == 0) 
+            {
+                throw new ArgumentNullException(nameof(file), "File is empty or null");
+            }
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(fileNameForStorage))
+                {
+                    fileNameForStorage = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+                }
+
+                string contentType = file.ContentType;
+                var options = new UploadObjectOptions();
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    await file.CopyToAsync(memoryStream);
+                    memoryStream.Position = 0;
+
+                    // Upload the file to Google Cloud Storage
+                    var storageObject = await _storageClient.UploadObjectAsync(_bucketName, fileNameForStorage, contentType, memoryStream, options);
+
+                    _logger.LogInformation($"Upload file {fileNameForStorage} to bucket {_bucketName}");
+
+                    // Return URL
+                    return $"https://storage.googleapis.com/{_bucketName}/{fileNameForStorage}";
+                }
+
+            } catch (Google.GoogleApiException gae)
+            {
+                _logger.LogError(gae, $"Google API Error during file upload: {gae.Message}");
+                throw new ApplicationException($"Google API Error during file upload: {gae.Message}");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Unexpected error during file upload: {e.Message}");
+                throw new ApplicationException($"Unexpected error during file upload: {e.Message}");
+            }
         }
     }
 }
